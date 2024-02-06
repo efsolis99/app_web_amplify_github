69ea2e60cb3733b691826b24c7a7b41a994e4cab
Efrain  Uriel Solis Salinas <efsolis@EFSOLISMID.local>
2024-02-06 11:43:17 -0600

    2 upload files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('ola')
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Nuevo texto</p>
         <a
           className="App-link"
           href="https://reactjs.org"
